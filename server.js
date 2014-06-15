2718a9b493870c3955ad6a633ef5a5bb5bf0382e
Infinite Flux Master <braungoodson@gmail.com>
2014-06-15 02:15:02 -0400

    added source

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express'),
+	cookieParser = require('cookie-parser'),
   server = express(),
   port = process.env.PORT || 30000,
   staticRoot = __dirname;
 
+server.use(cookieParser());
 server.use('/', express.static(staticRoot));
 server.listen(port);
 console.log('http://localhost:'+port);
